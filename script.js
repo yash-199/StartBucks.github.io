1f491e79c515cd2a2217dc3d48f2c3ff8ff5db9d
yash-199 <68182949+yash-199@users.noreply.github.com>
2022-10-20 21:13:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+let btn = document.getElementById('menu-btn')
+let nav = document.getElementById('menu')
+
+function navToggle(){
+    btn.classList.toggle('open')
+    nav.classList.toggle('hidden')
+    document.body.classList.toggle('no-scroll')
+}
+
+btn.addEventListener('click',navToggle)
+
+
+const accordionBtns = document.querySelectorAll(".accordion");
+
+accordionBtns.forEach((accordion) => {
+  accordion.onclick = function () {
+    this.classList.toggle("is-open");
+
+    let content = this.nextElementSibling;
+    console.log(content);
+
+    if (content.style.maxHeight) {
+      //this is if the accordion is open
+      content.style.maxHeight = null;
+    } else {
+      //if the accordion is currently closed
+      content.style.maxHeight = content.scrollHeight + "px";
+      console.log(content.style.maxHeight);
+    }
+  };
+});
